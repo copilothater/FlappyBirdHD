4a0648325876ee0bb6e0e006140e65aca038049a
Mini180 Cours2025 <ENGGm609@uottawa.ca>
2025-04-29 11:59:21 -0400

    FlappyBirdHD

---
+++ script.js
@@ -0,0 +1,29 @@
+let gravity = 0.25;
+let bird_dy=0;
+let score = 0;
+let game_state = "start";
+
+let gameInterval = null;
+
+let bird = document.getElementById("bird");
+let score_display = document.getElementById("score");
+let game_container = document.getElementById("game-container");
+let start_btn = document.getElementById("start_btn");
+
+function applyGravity() {
+    bird_dy += gravity;
+    let birdTop = bird.offsetTop + bird_dy;
+
+    birdTop = Math.max(birdTop, 0);
+        if (birdTop < 0) {birdTop = 0};
+    birdTop = Math.min(birdTop, game_container.offsetHeight - bird.offsetHeight);
+    bird.style.top = birdTop + "px";
+}
+
+document.addEventListener("keydown", (e)=> {
+    if (e.code === "Space" || e.code === "ArrowUp") {
+        if (game_state !== "Play") {
+            if (game_state) = "Play"
+        }
+    }
+})
